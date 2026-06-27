4c3eb16696bce48909d2b49179521a6be5ef1cc1
Andrea Galenda <andrea.galenda@gmail.com>
2026-06-27 16:30:59 -0700

    Delete Lab_Industrial_Robotics directory

--- Lab_Industrial_Robotics/main.py
+++
@@ -1,9 +0,0 @@
-from user_interface import UI
-import techman as tm
-from constants import *
-
-# This is the main file to be run to launch the user interface
-
-if __name__ == "__main__":
-    app = UI()
-    app.mainloop()
